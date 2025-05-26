44e1febde302972365bd4974a848da599b1cdf9b
Bharat Dev Burman <bharat.singh7924@gmail.com>
2025-05-26 16:04:04 +0530

    bump version to 0.1.0
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.crazo7924.onlymusic"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
