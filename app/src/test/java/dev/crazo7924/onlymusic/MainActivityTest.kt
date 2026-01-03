4b82e3b48076cd12998191e4ecf75cd0cf5c44cb
Bharat Dev Burman <bharat.singh7924@gmail.com>
2026-01-04 02:15:55 +0530

    Add test for MainActivity lifecycle state

--- app/src/test/java/dev/crazo7924/onlymusic/MainActivityTest.kt
+++ app/src/test/java/dev/crazo7924/onlymusic/MainActivityTest.kt
@@ -1,12 +1,20 @@
 package dev.crazo7924.onlymusic
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import androidx.lifecycle.Lifecycle
+import org.junit.Assert.*
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
-@ExperimentalCoroutinesApi
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [30])
 class MainActivityTest {
-
+    @Test
+    fun activity_lifecycle_resumed_after_setup() {
+        val activity = Robolectric.buildActivity(MainActivity::class.java).setup().get()
+        assertNotNull(activity)
+        assertEquals(Lifecycle.State.RESUMED, activity.lifecycle.currentState)
+    }
 }
