a6fa98f66271d8e1c4a70d8af021408d4561016f
Bharat Dev Burman <bharat.singh7924@gmail.com>
2026-01-18 18:18:53 +0530

    feat: bump version to 0.2.0 and update dependencies
    - Bumps app version:
        - versionCode: 4 -> 5
        - versionName: 0.1.2 -> 0.2.0
    - Updates NewPipe Extractor dependency: 0.24.8 -> v0.25.0
    
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ extensions.configure<ApplicationExtension>("android") {
         applicationId = "dev.crazo7924.onlymusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.2"
+        versionCode = 5
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
