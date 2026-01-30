2d690d699bfefd145e86d072d21dfc01cbbc054f
Bharat Dev Burman <bharat.singh7924@gmail.com>
2026-01-30 21:09:46 +0530

    bump version to 0.3.0
    versionCode: 6 -> 7
    versionName: 0.2.1 -> 0.3.0
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ extensions.configure<ApplicationExtension>("android") {
         applicationId = "dev.crazo7924.onlymusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.2.1"
+        versionCode = 7
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
